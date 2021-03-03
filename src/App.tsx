856b79a8f0ddf177191349201d4b9fb3b2305290
Cyriaque <cyriaquedelaunay@gmail.com>
2021-03-03 18:31:43 +0100

    //

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Link, Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Login from './components/login/Login';
 
 function App() {
   return (
@@ -10,15 +13,39 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/users"></Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './home.scss';
+
+function Home() {
+  return <div className="home"></div>;
+}
+
+export default Home;
